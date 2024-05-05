001203bc28254ee13cf2a8b2780821e5f32090da
WAYNE_SEA <zhou.zhiwe@northeastern.edu>
2024-05-05 12:29:12 -0700

    feat button+textarea frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <input type="text" />
-    </>
+    <div className="textarea-container">
+      <Textarea placeholder="Type your message here." />
+      <Button>Send message</Button>
+    </div>
   )
 }
 
