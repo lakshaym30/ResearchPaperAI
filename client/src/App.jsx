61246730c2a7f274f1a3cefa3a64947b69ef226a
drebel <drewbeland@gmail.com>
2024-05-05 12:30:24 -0700

    file upload still needs endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { Input } from "@/components/ui/input"
-
-import './App.css'
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 function App() {
 
+  const [file, setFile] = useState(null)
+
+  function handleFileChange(event){
+    setFile(event.target.files[0])
+  }
+
+  async function handleFileUpload(){
+    if(!file){
+      alert('Please select a file')
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('file', file)
+
+    try{
+      const response = await fetch('',{
+        method: 'POST',
+        body: formData
+      })
+
+      if(!response.ok){
+        throw new Error('Network response was not ok')
+      }
+
+      const data = await response.json()
+      console.log(data)
+
+    }catch(error){
+      console.error('There was a problem with your fetch operation', error)
+    }
+  }
+
+  // useEffect(() => {
+  //   console.log(file)
+  // },[file])s
+
   return (
     <>
-      <input type="text" />
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="fileUpload">File Upload</Label>
+        <Input id="fileUpload" type="file" onChange={handleFileChange}/>
+        <Button onClick={handleFileUpload}>Submit PDF</Button>
+
+      </div>    
     </>
   )
 }
