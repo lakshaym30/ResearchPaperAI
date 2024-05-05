48007f472edf80a39d0daded973c17ff9655a300
drebel <drewbeland@gmail.com>
2024-05-05 10:57:11 -0700

    added frontend branch

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import { Input } from "@/components/ui/input"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <input type="text" />
+    </>
+  )
+}
+
+export default App
